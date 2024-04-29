c6b6ef6cea927fcbcaa8f66ec48457037c87f088
PHANINDRA KUMAR NIDAMANURI <git config --global user.email>
2024-04-29 21:53:03 +0530

    props

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App"
+import "./index.css"
+
+
+// function PrintName() {
+//   return <h1>hello react js</h1>
+// }
+
+// const PrintName = () => {
+//   return <h1>hello, iam coming from arrow function components</h1>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
+//This is for the time render for every second 
+// setInterval(() => {
+//     root.render(<App />);
+// }, 1000);
+
+
